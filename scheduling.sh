08416a926c5ea0ae5f1824af3f74325c66d2270a
Migoreng <lv.kjsn.29yr@gmail.com>
2018-11-22 23:17:49 +0900

    pastget.shとscheduling.shを追加。ソース汚いけどちゃんと動く。

--- scheduling.sh
+++ scheduling.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
+#定常取得用
 mapjamname=$1
 X=$2
 Y=$3
 zoom=$4 #本来のZの値
 z=$(($zoom + 1))
+
+[ "$#" -eq 4 ] || ( echo '第一引数は保存地名、第二引数はX軸、第三引数はY軸、第四引数はズーム率Z' 1&>2 && exit 1 );
+
 
  if [ $zoom -eq 0 ]; then   Y=$Y ;fi
  if [ $zoom -eq 1 ]; then   Y=$((0 - $Y)) ;fi
@@ -34,47 +37,19 @@ y1=$Y
 y2=$(($y1 + 1))
 
 
-#==== START_DATEつくる ====#
-TODAY_FIRST=$(date -d '' --iso-8601=minutes)
-TODAY_FIRST=$(date -d "-7 days $TODAY_FIRST" --iso-8601=minutes)
-START_DATE=${TODAY_FIRST}
-echo "\$START_DATE:$START_DATE"
-DISPLAY_DATE=$(date -d"$TODAY_FIRST" "+%Y%m%d%H%M")
-echo "\$DISPLAY_DATE:$DISPLAY_DATE"
-
-switch=0
-DATE=${START_DATE}
-
-#=== END_DATE つくるよ===#
 if [ $(date "+%M") -lt 40 -a $(date "+%M") -gt 0 ];then
-END_DATE=$(date "+%Y%m%d%H00")
+date=$(date "+%Y%m%d%H00")
 fi
 if [ $(date "+%M") -le 59 -a $(date "+%M") -gt 40 ];then
-END_DATE=$(date "+%Y%m%d%H40")
-fi
-echo "$END_DATEだってばよ"
-#=========================#
-
-while [ 1 ];do
-
-DISPLAY_DATE=$(date -d "${DATE}" "+%Y%m%d%H%M")
-
-if [ ${DISPLAY_DATE} -gt ${END_DATE} ];then
-    break
+date=$(date "+%Y%m%d%H40")
 fi
 
 
-echo "===1==="
-echo "\$DISPLAY_DATE:$DISPLAY_DATE"
-echo "\$START_DATE:$START_DATE"
-echo "\$DATE:$DATE"
-echo "\$END_DATE:$END_DATE"
 
-
-curl -o jam_${x1}-${y1}-${z}.png -O "https://map.c.yimg.jp/jam?x=$x1&y=$y1&z=$z&date=${DISPLAY_DATE}&size=406"
-curl -o jam_${x1}-${y2}-${z}.png -O "https://map.c.yimg.jp/jam?x=$x1&y=$y2&z=$z&date=${DISPLAY_DATE}&size=406"
-curl -o jam_${x2}-${y1}-${z}.png -O "https://map.c.yimg.jp/jam?x=$x2&y=$y1&z=$z&date=${DISPLAY_DATE}&size=406"
-curl -o jam_${x2}-${y2}-${z}.png -O "https://map.c.yimg.jp/jam?x=$x2&y=$y2&z=$z&date=${DISPLAY_DATE}&size=406"
+curl -o jam_${x1}-${y1}-${z}.png -O "https://map.c.yimg.jp/jam?x=$x1&y=$y1&z=$z&date=$date&size=406"
+curl -o jam_${x1}-${y2}-${z}.png -O "https://map.c.yimg.jp/jam?x=$x1&y=$y2&z=$z&date=$date&size=406"
+curl -o jam_${x2}-${y1}-${z}.png -O "https://map.c.yimg.jp/jam?x=$x2&y=$y1&z=$z&date=$date&size=406"
+curl -o jam_${x2}-${y2}-${z}.png -O "https://map.c.yimg.jp/jam?x=$x2&y=$y2&z=$z&date=$date&size=406"
 
 if [ ! -s "mapmon_${x1}-${y2}-${z}_${x2}-${y2}-${z}_${x1}-${y1}-${z}_${x2}-${y1}-${z}.png" ];then 
 curl -o map_${x1}-${y1}-${z}.png -O "https://map.c.yimg.jp/m?r=1&x=$x1&y=$y1&z=$z&size=406"
@@ -88,18 +63,5 @@ fi
 
 montage jam_${x1}-${y2}-${z}.png jam_${x2}-${y2}-${z}.png jam_${x1}-${y1}-${z}.png jam_${x2}-${y1}-${z}.png -tile 2x2 -resize 100% -geometry +0+0 jammon_${x1}-${y2}-${z}_${x2}-${y2}-${z}_${x1}-${y1}-${z}_${x2}-${y1}-${z}.png
 
-composite -dissolve 50%x50% jammon_${x1}-${y2}-${z}_${x2}-${y2}-${z}_${x1}-${y1}-${z}_${x2}-${y1}-${z}.png mapmon_${x1}-${y2}-${z}_${x2}-${y2}-${z}_${x1}-${y1}-${z}_${x2}-${y1}-${z}.png ${mapjamname}_${DISPLAY_DATE}.png
+composite -dissolve 50%x50% jammon_${x1}-${y2}-${z}_${x2}-${y2}-${z}_${x1}-${y1}-${z}_${x2}-${y1}-${z}.png mapmon_${x1}-${y2}-${z}_${x2}-${y2}-${z}_${x1}-${y1}-${z}_${x2}-${y1}-${z}.png ${mapjamname}_${date}.png
 #rclone copy "com_${x}-${y}-${z}.png" "GoogleDrive:Google フォト/rclonetest"
-
-
-if [ $(( $switch % 2 )) -eq 0 ] ;then DATE=$(date -d "+40 minutes ${DATE}" --iso-8601=minutes) ;fi
-if [ $(( $switch % 2 )) -eq 1 ] ;then DATE=$(date -d "+20 minutes ${DATE}" --iso-8601=minutes) ;fi
-switch=$(( ${switch} + 1 ))
-
-echo "===2==="
-echo "\$switch:$switch"
-echo "\$DISPLAY_DATE:$DISPLAY_DATE"
-echo "\$START_DATE:$START_DATE"
-echo "\$DATE:$DATE"
-echo "\$END_DATE:$END_DATE"
-done
